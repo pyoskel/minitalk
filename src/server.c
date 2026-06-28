881af034c55b7259aae9f8e4d08d41d20767d37f
pabartoc <pabartoc@student.42heilbronn.de>
2026-06-28 09:29:15 +0200

    print the server PID

--- src/server.c
+++ src/server.c
@@ -6,13 +6,15 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 06:29:18 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/28 08:04:46 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/28 09:28:49 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include "ft_printf.h"
 
 int	main(void)
 {
+	ft_printf("Server PID = %d" getpid());
 	return (EXIT_SUCCESS);
 }
