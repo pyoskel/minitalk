a78932623a6d8fcd68162ccbe7a97dd1d4466e04
pabartoc <pabartoc@student.42heilbronn.de>
2026-06-28 07:57:45 +0200

    start to define the header and include librarys.

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,7 +6,15 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 06:29:13 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/26 06:29:14 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/28 05:26:32 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h> // for write
+# include <stdlib.h> // for EXIT_SUCCESS
+# include <signal.h> // for kill
+
+#endif
